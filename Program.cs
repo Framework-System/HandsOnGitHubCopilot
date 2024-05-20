8796c53a8317464a0c8b19d75d6fea8c3ab20727
Rafael Bonaldi <rafaelbonaldi@gmail.com>
2024-05-20 14:03:59 -0300

    metodo de soma

--- Program.cs
+++ Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+
+namespace HandsOnGitHubCopilot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numeros = new List<int> { 1, 2, 3, 4, 5 };
+            int soma = S(numeros);
+            Console.WriteLine($"A soma é: {soma}");
+        }
+
+        static int S(List<int> numeros)
+        {
+            // Implementação da função
+        }
+    }
+}

67c70571358b1b482ec98e937ebb040590c467dc
Rafael Bonaldi <rafaelbonaldi@gmail.com>
2024-05-20 14:05:15 -0300

    metodo de soma

--- Program.cs
+++ Program.cs
@@ -8,11 +8,11 @@ namespace HandsOnGitHubCopilot
         static void Main(string[] args)
         {
             List<int> numeros = new List<int> { 1, 2, 3, 4, 5 };
-            int soma = S(numeros);
+            int soma = Sm(numeros);
             Console.WriteLine($"A soma é: {soma}");
         }
 
-        static int S(List<int> numeros)
+        static int Sm(List<int> numeros)
         {
             // Implementação da função
         }
